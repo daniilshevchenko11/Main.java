f66eea592157571bd91964b19eff1e86d5d74dca
daniilshevchenko11 <mrdaniilshevchenko@gmail.com>
2022-08-10 20:15:32 +0300

    import java.util.Scanner; public class Main {     public static void main(String[] args) {         example1();         example2();     }
        private static void example1(){
            Scanner input = new Scanner(System.in);
            System.out.print("Enter x");
            double x = input.nextDouble();
            System.out.print("Enter y");
            double y = input.nextDouble();
            double z = Math.pow(x,y);
            System.out.println("z="+z);
        }
        private static void example2(){
            Scanner input = new Scanner(System.in);
            System.out.print("Enter x>=0");
            double x = input.nextDouble();
            if (x>=0) {
                double z = Math.sqrt(x);
                System.out.println("z=" + z);
            }
            else {
                System.out.println("You must have entered x>=0");
            }
    
        }
    
    }

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        example1();
+        example2();
     }
+
+    private static void example1(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter x");
+        double x = input.nextDouble();
+        System.out.print("Enter y");
+        double y = input.nextDouble();
+        double z = Math.pow(x,y);
+        System.out.println("z="+z);
+    }
+    private static void example2(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter x>=0");
+        double x = input.nextDouble();
+        if (x>=0) {
+            double z = Math.sqrt(x);
+            System.out.println("z=" + z);
+        }
+        else {
+            System.out.println("You must have entered x>=0");
+        }
+
+    }
+
+
+
 }
